5f9a84bdee8e04ed91493b652edf7731b61350bf
Susanna Boberg <sboberg617@gmail.com>
2024-11-23 18:32:49 -0500

    basic note taking (with styling)

--- basic sideline/sidepanel.js
+++ basic sideline/sidepanel.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+const save = document.getElementById("save");
+const clear = document.getElementById("clear");
+const noteInput = document.getElementById("noteInput");
+const create = document.getElementById("create")
+const notesContainer = document.getElementById("notes-container")
+let notes = document.querySelectorAll(".input-box")
+
+(function checkOldData() {
+    getData(function (data) {
+      noteInput.value = data
+    });
+    console.log(1);
+  })();
+
+  noteInput.addEventListener('keyup', async function() {
+    if (noteInput.value === "") return;
+    chrome.storage.local.set({ myData: noteInput.value });
+  })
+
+  save.addEventListener("click", async () => {
+    if (noteInput.value === "") return;
+    chrome.storage.local.set({ myData: noteInput.value });
+  });
+
+  async function getData(callback) {
+    chrome.storage.local.get(["myData"], function (result) {
+      let savedData = result.myData || "";
+      if (callback) {
+        callback(savedData);
+      }
+    });
+  }
